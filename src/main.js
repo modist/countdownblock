115088abcc5b7acc5eaca53bb73bc5a6fdf7322a
Steensgaard, Morten <morten.steensgaard@wunderman.com>
2018-04-29 22:56:34 +0200

    Fixed

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-l
 var SDK = require('blocksdk');
 var sdk = new SDK();
 
-var address, width, height, zoom, link, mapsKey;
+var day, month, year;
 
 function debounce (func, wait, immediate) {
 	var timeout;
@@ -21,53 +21,35 @@ function debounce (func, wait, immediate) {
 }
 
 function paintSettings () {
-	document.getElementById('text-input-id-1').value = address;
-	document.getElementById('slider-id-01').value = width;
-	document.getElementById('slider-id-02').value = height;
-	document.getElementById('slider-id-03').value = zoom;
+	document.getElementById('text-input-day').value = day;
+	document.getElementById('text-input-month').value = month;
+	document.getElementById('text-input-year').value = year;
 } 
 
-function paintSliderValues () {
-	document.getElementById('slider-id-01-val').innerHTML = document.getElementById('slider-id-01').value;
-	document.getElementById('slider-id-02-val').innerHTML = document.getElementById('slider-id-02').value;
-	document.getElementById('slider-id-03-val').innerHTML = document.getElementById('slider-id-03').value;
-}
-
 function paintMap() {
-	address = document.getElementById('text-input-id-1').value;
-	width = document.getElementById('slider-id-01').value;
-	height = document.getElementById('slider-id-02').value;
-	zoom = document.getElementById('slider-id-03').value;
-	link = document.getElementById('text-input-id-2').value;
-	if (!address) {
-		return;
-	}
-	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
-		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
-		'&markers=' + address.split(' ').join('+') + '&key=AIzaSyBViM0Y8LdxJYXWpyImtsXQBPi63n0ocfQ';
-	sdk.setContent('<a href="' + link + '"><img src="' + url + '" /></a>');
+	day = document.getElementById('text-input-day').value;
+	month = document.getElementById('slider-month').value;
+	year = document.getElementById('slider-year').value;
+
+	var url = 'https://img1.niftyimages.com/sgf/7km/t2b?dt=4/30/2018' + month + "/" + day + "/" + year;
+
+	sdk.setContent('<img src="' + url + '" />');
 	sdk.setData({
-		address: address,
-		width: width,
-		height: height,
-		zoom: zoom,
-		link: link,
-		mapsKey: mapsKey
+		day: day,
+		month: month,
+		year: year
 	});
 }
 
 sdk.getData(function (data) {
-	address = data.address || '';
-	width = data.width || 400;
-	height = data.height || 300;
-	zoom = data.zoom || 15;
-	link = data.link || '';
+	day = data.day || 31;
+	month = data.month || 12;
+	year = data.height || 2020;
+	zoom = data.year || 15;
 	paintSettings();
-	paintSliderValues();
 	paintMap();
 });
 
 document.getElementById('workspace').addEventListener("input", function () {
 	debounce(paintMap, 500)();
-	paintSliderValues();
 });
